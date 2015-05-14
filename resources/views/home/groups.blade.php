7b747e8f20c913505e1fa413664777db2e17636e
Vlad <vladislafus@gmail.com>
2015-05-14 21:42:51 +0300

    Basic application

---
+++ resources/views/home/groups.blade.php
@@ -0,0 +1,9 @@
+@extends('app')
+@section('content')
+
+		<div class="container">
+			<div class="content">
+				<div class="title">Групи</div>
+			</div>
+		</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+@section('content')
+
+		<div class="container">
+			<div class="content">
+				<div class="title">Модуль обліку роботи студента</div>
+				<div class="quote">{{ Inspiring::quote() }}</div>
+			</div>
+		</div>
+@endsection
